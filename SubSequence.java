c46537f661ca2906b795cb634beb0a07cfe81515
Vikram Kumar <vk8969338090@gmail.com>
2023-11-04 10:41:03 +0530

    new commit

--- SubSequence.java
+++ SubSequence.java
@@ -2,4 +2,13 @@ public class SubSequence {
     public static void main(String[] args) {
         
     }
+      static void subseq(String processed, String unprocessed) {
+        if(unprocessed.isEmpty()) {
+            System.out.println(processed);
+            return;
+        }
+        char ch = unprocessed.charAt(0);
+      }
+
+       
 }
