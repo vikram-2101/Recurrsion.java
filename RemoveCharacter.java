a8a01c7eef424447ae582fd15f91a16d0aeea256
Vikram Kumar <vk8969338090@gmail.com>
2023-11-04 10:32:59 +0530

    new commit

--- RemoveCharacter.java
+++ RemoveCharacter.java
@@ -1,7 +1,9 @@
+
 public class RemoveCharacter {
     public static void main(String[] args) {
-        skip("", "baccadah");
-
+      //  skip("", "baccadah");
+      //System.out.println(skip("baccadah"));
+     System.out.println(skipAppNotApple("bacappledah"));
     }
 
     static void skip(String processed, String Unprocessed) {
@@ -17,4 +19,28 @@ public class RemoveCharacter {
             skip(processed + ch, Unprocessed.substring(1));
         }
     }
+    // inPlace
+     static String skip(String unprocessed) {
+        if(unprocessed.isEmpty()) return "";
+
+        char ch = unprocessed.charAt(0);
+        if(ch == 'a') {
+            return skip(unprocessed.substring(1));
+
+        }
+        else {
+            return ch + skip(unprocessed.substring(1));
+        }
+     }
+     static String skipAppNotApple(String unprocessed) {
+          if(unprocessed.isEmpty()) return "";
+
+          if(unprocessed.startsWith("app")  && !unprocessed.startsWith("apple")) {
+            return skipAppNotApple(unprocessed.substring(5));
+          }
+          else {
+            return unprocessed.charAt(0)+skipAppNotApple(unprocessed.substring(1));
+          }
+     }
+
 }
