8bb6692744be74b3b00529681d74833c505aacd7
Vikram Kumar <vk8969338090@gmail.com>
2023-11-06 17:23:53 +0530

    new

--- BubbleSort.java
+++ BubbleSort.java
@@ -4,6 +4,8 @@ public class BubbleSort {
         int arr[] = {4,3,2,1};
         bubble(arr, arr.length-1, 0);
        System.out.println(Arrays.toString(arr));
+       selection(arr, arr.length, 0, 0);
+       System.out.println(Arrays.toString(arr));
     }
      static void bubble(int[] arr , int r , int c) {
         if(r==0) return ;
@@ -19,5 +21,24 @@ public class BubbleSort {
         else {
             bubble(arr, r-1, c);
         }
+     }
+     static void selection(int[] arr , int r, int c , int max) {
+        if(r==0) return;
+        if(c<r) {
+            if(arr[c]> arr[max])  {
+                selection(arr, r, c+1, c);
+            }
+            else {
+                selection(arr, r, c+1, max);
+            }
+            
+        }
+        else {
+                
+                int temp = arr[max];
+                 arr[max] = arr[r-1] ;
+                arr[r-1] = temp;
+                selection(arr, r-1, 0,0);
+            }
      }
 }
