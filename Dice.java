83210525f49c7cf3adeec81dc2d14ed459415387
Vikram Kumar <vk8969338090@gmail.com>
2023-11-08 22:16:37 +0530

    new commit

---
+++ Dice.java
@@ -0,0 +1,17 @@
+public class Dice {
+    public static void main(String[] args) {
+        
+    }
+    static void dice(String p , int target) {
+        if(target ==0) {
+            System.out.println(p);
+            return;
+        }
+
+        for(int i = 1; i<=6&& i<=target; i++) {
+      dice(p+i, target-i);
+        }
+
+    }
+    
+}
