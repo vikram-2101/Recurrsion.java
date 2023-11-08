460e3269cfc64351ef4ee93db58ac6b985e78709
Vikram Kumar <vk8969338090@gmail.com>
2023-11-08 21:50:13 +0530

    counting the number of ans

--- LetterCombination.java
+++ LetterCombination.java
@@ -3,7 +3,8 @@ import java.util.ArrayList;
 public class LetterCombination {
     public static void main(String[] args) {
          // phonepad("", "12");
-        System.out.println(pad("", "34"));
+        System.out.println(padcount("", "23"));
+         //System.out.println(pad("", "34"));
     }
 
     static void phonepad(String p, String up) {
@@ -37,5 +38,18 @@ public class LetterCombination {
        }
        return list;
     }
+
+     static int padcount(String p , String up)  {
+        if(up.isEmpty())  {
+            return 1;
+        }
+        int count = 0;
+        int digit = up.charAt(0)- '0';
+        for(int i =  (digit - 1) * 3; i < digit * 3; i++) {
+            char ch = (char) ('a' + i);
+            count = count + padcount(p+ch, up.substring(1));
+        }
+        return count;
+     }
 
 }
