230a68108b98dbe2b21b7b6909b60e02db727018
Vikram Kumar <145278357+vikram-2101@users.noreply.github.com>
2023-11-30 18:43:03 +0530

    Update AllPath.java

--- BackTracking.java/AllPath.java
+++ BackTracking.java/AllPath.java
@@ -2,7 +2,7 @@
 public class AllPath {
     public static void main(String[] args) {
         boolean[][] board = {
-                { true, true, true },
+                { true, true, true },d
                 { true, true, true },
                 { true, true, true },
         };
